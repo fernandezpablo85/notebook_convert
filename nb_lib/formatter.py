49ed85ed214eea7718f37b5d24216fc5f5e305dc
Pablo Fernandez <fernandezpablo85@gmail.com>
2020-04-16 23:18:23 -0300

    refactor nb formatting and generation

--- nb_lib/formatter.py
+++ nb_lib/formatter.py
@@ -1,5 +1,59 @@
+import os
 import nbformat
-from nbconvert import MarkdownExporter
+from nbconvert import MarkdownExporter, PDFExporter
+from nbconvert.writers import FilesWriter
 
-def convert(file):
-    pass
+SUPPORTED_FORMATS = {"md", "pdf"}
+
+
+class Formatter:
+    def __init__(self, output):
+        assert output in SUPPORTED_FORMATS, f"supported formats are {SUPPORTED_FORMATS}"
+        self.read_encoding = "utf-8"
+        self.write_encoding = "utf-8"
+        self.format = output
+
+        if self.format == "pdf":
+            pdf = PDFExporter()
+            pdf.exclude_output_prompt = True
+            pdf.exclude_input = True
+            self.exporter = pdf
+        else:
+            self.exporter = MarkdownExporter()
+
+    def convert(self, file):
+        assert os.path.exists(file), f"this should not happen, path {file} must exist"
+        body, resources = self.export(file)
+
+        fw = FilesWriter()
+        fw.write(body, resources, notebook_name=file.replace(".ipynb", ""))
+
+    def dst_path(self, file):
+        return file.replace(".ipynb", f".{self.format}")
+
+    def export(self, file):
+        with open(file, "r", encoding=self.read_encoding) as f:
+            nb = nbformat.read(f, as_version=4)
+            body, resources = self.exporter.from_notebook_node(nb)
+            return body, resources
+
+    def needs_format(self, file):
+        f_path = self.dst_path(file)
+
+        if not os.path.exists(f_path):
+            return True
+
+        notebook_modified = os.stat(file).st_mtime
+        formatted_modified = os.stat(f_path).st_mtime
+
+        return notebook_modified > formatted_modified
+
+    def save_figures(self, resources):
+        if "outputs" not in resources:
+            return
+
+        for name, bytes_ in resources["outputs"]:
+            print(f"name = {name}, bytes = {len(bytes_)}")
+
+        for key, value in resources.items():
+            pass
--- nb_lib/tool.py
+++ nb_lib/tool.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python
-import sys
-import os
-from .git import is_staged
-from .formatter import convert
-
-SUPPORTED_FORMATS = "md"
+from .formatter import Formatter
 
 
 def main(args):
     notebooks = args[1:]
-    to_format = "md"
-    converted = [n for n in notebooks if convert(n, to_format=to_format)]
-    none_converted = len(converted) == 0
-    if len(converted) != 0:
-        for c in converted:
-            print(f"{c} {to_format} missing, had to be generated")
+    fmt = Formatter(output="pdf")
+    converted = [n for n in notebooks if convert(fmt, file=n)]
+    if converted:
         exit(1)
 
-    non_staged = [n for n in notebooks if not _is_staged_with_conversion(n, to_format)]
+    non_staged = [n for n in notebooks if not is_staged_with_conversion(fmt, file=n)]
     for non_s in non_staged:
-        print(f"{non_s} {to_format} representation is not staged")
+        print(f"{fmt.dst_path(non_s)} is not staged")
 
     if non_staged:
         exit(1)
@@ -27,53 +19,25 @@ def main(args):
     exit(0)  # this is fine
 
 
-def _convertion_format(notebook: str, to_format: str) -> str:
-    return notebook.replace(".ipynb", f".{to_format}")
-
-
-def convert(notebook: str, to_format="rst") -> bool:
-    assert to_format in SUPPORTED_FORMATS, f"supported formats are {SUPPORTED_FORMATS}"
-    assert os.path.exists(notebook), f"this should not happen, path {notebook} must exist"
-    if not must_convert(notebook, to_format):
+def convert(fmt: Formatter, file: str) -> bool:
+    if not fmt.needs_format(file):
         return False
 
-    try:
-        do_convert(notebook, to_format)
-    except Exception:
-        return False
+    print(f"{fmt.dst_path(file)} is outdated or inexistent, converting... ", end="")
+    fmt.convert(file)
+    print("OK")
+
     return True
 
 
-def must_convert(notebook: str, to_format: str) -> bool:
-    formatted_path = _convertion_format(notebook, to_format)
+def is_staged_with_conversion(fmt: Formatter, file: str) -> bool:
+    from .git import is_staged
 
-    if not os.path.exists(formatted_path):
-        return True
-
-    notebook_modified = os.stat(notebook).st_mtime
-    formatted_modified = os.stat(formatted_path).st_mtime
-
-    return notebook_modified > formatted_modified
-
-
-def do_convert(notebook, to_format):
-    print(f"{notebook} {to_format} format is outdated or inexistent, converting ...")
-    with open(notebook, "r", encoding="utf-8") as f:
-        nb = nbformat.read(f, as_version=4)
-        body, resources = MarkdownExporter().from_notebook_node(nb)
-        to_path = _convertion_format(notebook, to_format)
-        with open(to_path, "w+", encoding="utf-8") as to_file:
-            to_file.write(body)
-        for key, value in resources.items():
-            print(key)
-            print(value)
-    print(f"generated {to_path}")
-
-
-def _is_staged_with_conversion(notebook, to_format):
-    out_file = _convertion_format(notebook, to_format)
-    return is_staged(out_file) and is_staged(notebook)
+    out_file = fmt.dst_path(file)
+    return is_staged(file) and is_staged(out_file)
 
 
 if __name__ == "__main__":
+    import sys
+
     main(sys.argv)
